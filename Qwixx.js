2a2dcb1c99b13e7c5e319a9720d1c8c3d1e34647
hubinstein <rubinsteinbenjamin993@gmail.com>
2025-04-12 18:41:16 -0400

    Update Qwixx.js

--- Qwixx.js
+++ Qwixx.js
@@ -181,10 +181,9 @@ function redChoiceMark() {
 
 }
 function yellowChoiceMark(){
-  if (mark('yellow', parseInt(dice.white.innerText)+parseInt(dice.white2.innerText))!='failed'){
-    removeListeners();
-  }
-}
+  mark('yellow', parseInt(dice.white.innerText)+parseInt(dice.white2.innerText));
+  removeListeners();
+  tutorial.innerText ='select a white die, take a penalty to roll, or if you have made a move, you can roll without penalty';
 function blueChoiceMark(){
   mark('blue', parseInt(dice.white.innerText)+parseInt(dice.white2.innerText))
   removeListeners();
